58cf8d4c92984934ad92326c68ff0c5ebf91e12f
Charlie Kenney <charlesc.kenney@gmail.com>
2018-01-24 19:32:33 -0500

    finish user tests

--- test/controllers/user.js
+++ test/controllers/user.js
@@ -24,6 +24,8 @@ const userBuffer3 = {
   lastName: 'ekfjowjefo',
 }
 
+const tokens = []
+
 const validUsers = [userBuffer1, userBuffer3]
 
 function omitPassword(userBuffer) {
@@ -43,6 +45,7 @@ describe('POST /users', () => {
         if (err) return done(err)
         expect(res.body.newUser).to.shallowDeepEqual(omitUserPassWord(userBuffer1))
         expect(res.body.hasOwnProperty('token'), true)
+        tokens[1] = `Bearer ${res.body.token}`
         done()
       })
   })
@@ -69,6 +72,7 @@ describe('POST /users', () => {
         if (err) return done(err)
         expect(res.body.newUser).to.shallowDeepEqual(omitUserPassWord(userBuffer3))
         expect(res.body.hasOwnProperty('token'), true)
+        tokens[3] = `Bearer ${res.body.token}`
         done()
       })
   })
@@ -88,8 +92,65 @@ describe('GET /users', () => {
 })
 
 describe('PUT /users/:userName', () => {
-  it('should fail because user is not an admin or current user')
+  it('should fail because user is not an admin or current user', done => {
+    request(app)
+      .put(`/users/${userBuffer1.userName}`)
+      .set('Authorization', tokens[3])
+      .send({ userName: userBuffer1.userName, passWord: userBuffer1.passWord })
+      .expect(401)
+      .end((err, res) => {
+        if (err) return done(err)
+        done()
+      })
+  })
 
-  it('should allow user to change profile')
+  it('should allow user to change profile', done => {
+    request(app)
+      .put(`/users/${userBuffer3.userName}`)
+      .set('Authorization', tokens[3])
+      .send({ userName: 'foo' })
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err)
+        expect(res.body.updatedUser).to.shallowDeepEqual({ ...omitUserPassWord(userBuffer3), userName: 'foo' })
+        done()
+      })
+  })
 })
 
+describe('GET /users/:userName', () => {
+  it('should allow user to see profile', done => {
+    request(app)
+      .get(`/users/${userBuffer1.userName}`)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err)
+        expect(res.body.user).to.shallowDeepEqual(omitUserPassWord(userBuffer1))
+        done()
+      })
+  })
+})
+
+describe('DELETE /users/:userName', () => {
+  it('should throw unauthorized when not admin or current user', done => {
+    request(app)
+      .delete('/users/non-existant')
+      .expect(401)
+      .end((err, res) => {
+        if (err) return done(err)
+        done()
+      })
+  })
+
+  it('should allow user to delete', done => {
+    request(app)
+      .delete(`/users/${userBuffer1.userName}`)
+      .set('Authorization', tokens[1])
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err)
+        expect(res.body.deletedUser).to.shallowDeepEqual(omitUserPassWord(userBuffer1))
+        done()
+      })
+  })
+})
