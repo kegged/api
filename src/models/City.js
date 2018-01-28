9ebb4724ff79ecb33604f3b0ac55bd653ec56aba
Charlie Kenney <charlesc.kenney@gmail.com>
2018-01-28 00:59:43 -0500

    add city url slugs

--- src/models/City.js
+++ src/models/City.js
@@ -1,10 +1,20 @@
+import slugify from 'sequelize-slugify'
+
 export default (sequelize, DataTypes) => {
   const City = sequelize.define('City', {
     name: {
       type: DataTypes.STRING,
       unique: true,
       allowNull: false,
+    },
+    slug: {
+      type: DataTypes.STRING,
+      unique: true,
     }
+  })
+
+  slugify.slugifyModel(City, {
+    source: ['name']
   })
 
   City.associate = models => {
